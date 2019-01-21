ac7da6b4d6d278542b9f913bd802cc1de2c318fa
evoblackshark <kilowan2@gmail.com>
2019-01-21 16:38:35 +0100

    Cambio Hola mundo

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola mi mundo");
         }
     }
 }
